e6a9decccc0952c33af81570b192426f2a718f85
Anh Le <anhle_2021@depauw.edu>
2020-05-31 18:21:45 -0400

    Added 20x50 Grid

---
+++ src/Visualizer/PathFindingVisualizer.jsx
@@ -0,0 +1,39 @@
+import React, {Component} from 'react';
+import Node from './Node/Node';
+import './PathFindingVisualizer.css';
+import { directive } from '@babel/types';
+
+export default class PathfindingVisualizer extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            grid: [], //mouseIsPressed: false,
+        };
+    }
+    componentDidMount(){
+        const grid = [];
+        for(let row = 0; row < 20; row++){
+            const currRow = [];
+            for(let col = 0; col < 50; col++){
+                currRow.push([]);
+            }
+            grid.push(currRow);
+        }
+        this.setState({grid});
+    }
+    render(){
+        const {grid} = this.state;
+        console.log(grid)
+        return (
+            <div> 
+                Welcome to Path Finding Visualizer!
+                {grid.map((row, rowIndex) => {
+                    return <div>
+                        {row.map((node, nodeIndex) => <Node></Node>)}
+                    </div>
+                })}
+            </div>
+        );
+    }
+}
+
